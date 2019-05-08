f9eb64c965915379b94c332ff7102a63e2c1ed98
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-08 09:07:22 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,15 +19,7 @@ def first_challenge
 contacts.each do |person, data|
   
   data.each do |attribute, value|
-    
-    if attribute == favorite_icecream_flavors 
-      
-      value.delete_if{|flavour| flavour == "strawberry"}
-    end
-    
-  end
-end
-
+ 
   contacts
 end
 
